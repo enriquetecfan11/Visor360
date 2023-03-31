bdca18fb5b2f4008d69258775fd0fa15b575cd39
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-03-31 18:28:21 +0200

    Remove

--- guardar-imagen.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_FILES['imagen']) {
-  $carpeta = 'assets/';
-  $archivo = basename($_FILES['imagen']['name']);
-  $ruta = $carpeta . $archivo;
-  move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta);
-}
-?>
